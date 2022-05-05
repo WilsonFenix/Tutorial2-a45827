da18ad47c49a765b9f37221af9e32266902722d6
NoLogicTech <80648342+NoLogicTech@users.noreply.github.com>
2022-05-05 14:23:11 +0100

    5.Make a Vector

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Vector3 m_Movement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame

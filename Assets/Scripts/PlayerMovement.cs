39db61cfc814407f16085e1fb24c2d45d16900c2
NoLogicTech <80648342+NoLogicTech@users.noreply.github.com>
2022-05-06 11:32:00 +0100

    13.Update your PlayerMovement script to play audio

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,11 +10,13 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
+    AudioSource m_AudioSource;
 
     void Start()
     {
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
     void FixedUpdate()
